e83fb01ee38f9a6a7ac3f6c18953654568da1411
Tim Brown <bittebrown@gmail.com>
2025-04-04 09:51:05 +0100

    Map jk to Esc in insert mode

--- lua/custom-keymaps.lua
+++ lua/custom-keymaps.lua
@@ -1,4 +1,10 @@
 return {
+  -- Insert Mode
+  -- Set 'jk' to Esc. This reduces the need to stretch. In othe modes it is a noop (down,up). 
+  vim.keymap.set('i', 'jk', '<Esc>')
+  
+    
+  -- Visual Mode
   -- Ctrl-r: easily search and replace selected text in document.
   vim.keymap.set("v", "<C-r>", "hy:%s/<C-r>h//gc<left><left><left>"),
 }
